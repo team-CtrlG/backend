8a638fb5dc02121edaec30f9072debb9bf63e2e7
leestana01 <leestana01@naver.com>
2026-01-12 17:46:06 +0900

    init : 초기 프로젝트 커밋

---
+++ src/main/java/ctrlg/gyeongdodat/GyeongdodatApplication.java
@@ -0,0 +1,13 @@
+package ctrlg.gyeongdodat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GyeongdodatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GyeongdodatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ctrlg/gyeongdodat/GyeongdodatApplicationTests.java
@@ -0,0 +1,13 @@
+package ctrlg.gyeongdodat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GyeongdodatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
