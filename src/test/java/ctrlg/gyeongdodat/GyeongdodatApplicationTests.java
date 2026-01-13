a0158d3b8fd2969891aac451f4af60bd5977f0b8
leestana01 <leestana01@naver.com>
2026-01-13 17:07:53 +0900

    feat: 통합테스트 비활성화

--- src/test/java/ctrlg/gyeongdodat/GyeongdodatApplicationTests.java
+++ src/test/java/ctrlg/gyeongdodat/GyeongdodatApplicationTests.java
@@ -1,8 +1,10 @@
 package ctrlg.gyeongdodat;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Disabled("통합테스트 제외 - DB/Redis 연결 필요")
 @SpringBootTest
 class GyeongdodatApplicationTests {
 
