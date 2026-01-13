720a5f7450367127f841c97542b17da9b713b698
leestana01 <leestana01@naver.com>
2026-01-13 17:25:51 +0900

    feat: RedisConfig 환경변수 설정

--- src/main/java/ctrlg/gyeongdodat/global/config/RedisConfig.java
+++ src/main/java/ctrlg/gyeongdodat/global/config/RedisConfig.java
@@ -1,9 +1,34 @@
 package ctrlg.gyeongdodat.global.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 @Configuration
 @EnableRedisRepositories
 public class RedisConfig {
+
+    @Value("${spring.data.redis.host}")
+    private String host;
+
+    @Value("${spring.data.redis.port}")
+    private int port;
+
+    @Value("${spring.data.redis.password:}")
+    private String password;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration redisConfig = new RedisStandaloneConfiguration();
+        redisConfig.setHostName(host);
+        redisConfig.setPort(port);
+        if (password != null && !password.isEmpty()) {
+            redisConfig.setPassword(password);
+        }
+        return new LettuceConnectionFactory(redisConfig);
+    }
 }
