8199eb4022fcd7d97bd3cd9dc57674e0bbfc14a8
parksomii <somi020605@gmail.com>
2026-01-14 00:44:10 +0900

    config: WebSocket STOMP 브로커 및 엔드포인트 설정 추가

---
+++ src/main/java/ctrlg/gyeongdodat/global/config/WebSocketConfig.java
@@ -0,0 +1,28 @@
+package ctrlg.gyeongdodat.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+	@Override
+	public void configureMessageBroker(MessageBrokerRegistry registry) {
+		// 서버 → 클라이언트로 보내는 채널
+		registry.enableSimpleBroker("/send");
+
+		// 클라이언트 → 서버로 보내는 채널
+		registry.setApplicationDestinationPrefixes("/app");
+	}
+
+	@Override
+	public void registerStompEndpoints(StompEndpointRegistry registry) {
+		registry.addEndpoint("/ws")
+			.setAllowedOriginPatterns("*");
+		//.withSockJS();
+	}
+}
