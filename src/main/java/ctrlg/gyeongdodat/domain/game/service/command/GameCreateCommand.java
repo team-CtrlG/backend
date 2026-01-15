71c6288c0ff39488c3d04fcae3f8dc6d16d467c8
parksomii <somi020605@gmail.com>
2026-01-15 11:10:31 +0900

    fix: WebSocket payload 역직렬화를 위한 기본 생성자 추가

--- src/main/java/ctrlg/gyeongdodat/domain/game/service/command/GameCreateCommand.java
+++ src/main/java/ctrlg/gyeongdodat/domain/game/service/command/GameCreateCommand.java
@@ -1,36 +1,40 @@
 package ctrlg.gyeongdodat.domain.game.service.command;
 
 import ctrlg.gyeongdodat.domain.game.entity.GameRedis;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
 import java.math.BigDecimal;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class GameCreateCommand {
 
-    private Integer gameTimeSec;
+	private Integer gameTimeSec;
 
-    private Integer hideTimeSec;
+	private Integer hideTimeSec;
 
-    private BigDecimal jailLat;
+	private BigDecimal jailLat;
 
-    private BigDecimal jailLng;
+	private BigDecimal jailLng;
 
-    private String jailImage;
+	private String jailImage;
 
-    private String rulesJson;
+	private String rulesJson;
 
-    public GameRedis toEntity(String id) {
-        return GameRedis.builder()
-                .id(id)
-                .gameTimeSec(gameTimeSec)
-                .hideTimeSec(hideTimeSec)
-                .jailLat(jailLat)
-                .jailLng(jailLng)
-                .jailImage(jailImage)
-                .rulesJson(rulesJson)
-                .build();
-    }
+	public GameRedis toEntity(String id) {
+		return GameRedis.builder()
+			.id(id)
+			.gameTimeSec(gameTimeSec)
+			.hideTimeSec(hideTimeSec)
+			.jailLat(jailLat)
+			.jailLng(jailLng)
+			.jailImage(jailImage)
+			.rulesJson(rulesJson)
+			.build();
+	}
 }
